3d5982976015bb05fdefaa7e3dd2befe94c159c9
angelinalin <angelina_ljy@163.com>
2021-03-22 13:49:45 +0800

    improve 等差 use for loop

--- 20210307/等差数列2.py
+++ 20210307/等差数列2.py
@@ -1,14 +1,10 @@
 def arith_progress(first_number, com_dif_num, num_nums):
-    a = 1
     list_ap = []
-    while True:
-        list_ap.append(first_number)
-        first_number += com_dif_num
-        a += 1
-        if a == num_nums + 1:
-            return list_ap
-            break
-
+    a = first_number
+    for num in range(0, num_nums):
+        list_ap.append(a)
+        a += com_dif_num
     return list_ap
 
-print(arith_progress(1,5,5))
+
+print(arith_progress(1, 5, 5))
