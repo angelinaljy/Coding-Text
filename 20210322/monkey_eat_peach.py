fdbe23aed4d4cd1a5bc5d24874e1d9fcd34e2400
angelinalin <angelina_ljy@163.com>
2021-03-22 20:54:27 +0800

    update new version 2.0 of narcissistic_number.py 20210322

---
+++ 20210322/monkey_eat_peach.py
@@ -0,0 +1,16 @@
+# def amount_peach(nums_peach_left, nums_days):
+#     for num_day in range(1,nums_days):
+#         nums_peach_left = (nums_peach_left + 1) * 2
+#     return nums_peach_left
+#
+#
+# print(amount_peach(1, 10))
+
+def amount_peach(n):
+    if n == 1:
+        return 1
+    else:
+        return (amount_peach(n-1)+1)*2
+
+
+print(amount_peach(10))
