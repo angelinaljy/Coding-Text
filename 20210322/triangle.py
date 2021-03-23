86b699bec1fcee030a0d29b2061b88b2f9a5ed3b
angelinalin <angelina_ljy@163.com>
2021-03-23 20:32:49 +0800

    first try - triangle.py

---
+++ 20210322/triangle.py
@@ -0,0 +1,19 @@
+# def triangle(n):
+    # for num_a in range(1,n+1):
+    #     print(" "*(n - num_a), end="")
+    #     print("* "*(num_a-1) + "*")
+
+
+def triangle(n):
+    for num_a in range(1,n+1):
+        print(" "*(n - num_a), end="")
+        for num_b in range(1,num_a):
+            print("* ", end="")
+        print("*")
+
+if __name__=="__main__":
+    triangle(5)
+    triangle(50)
+
+
+
