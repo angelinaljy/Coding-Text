337d00cf534af68e00ba6309c06cb22fe8514efb
angelinalin <angelina_ljy@163.com>
2021-03-28 18:56:33 +0800

    20210328 1.0 version - input_quit.py

---
+++ 20210328/input_quit.py
@@ -0,0 +1,14 @@
+# 从控制台输入一系列的字符或数字，把这些逐个写入到文件中，直到连续输入了quit后则停止程序；
+
+def input_quit():
+    answers = []
+    while True:
+        answer = input("Please write down anything: (Quit the question by 'quit')")
+        answers.append(answer)
+        if answer == "quit":
+            print(answers)
+            break
+
+
+if __name__ == "__main__":
+    input_quit()
