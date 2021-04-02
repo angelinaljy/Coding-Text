25945b1e189844b8264b0592ee6ad9a0963f4942
angelinalin <angelina_ljy@163.com>
2021-04-03 00:18:00 +0800

    2nd try 20210402

--- 20210328/class_info_statistics.py
+++ 20210328/class_info_statistics.py
@@ -1,5 +1,3 @@
-from numpy import *
-
 # 用字典存储一个班的学生信息（学号，姓名，性别，年龄，总分等），统计本班学生年龄最小的/最大的、分数最高的/最低的学生的信息，并输出到控制台；请求出男生的平均年龄、平均分；女生的平均年龄平均分；请求出本班的平均年龄、平均分。
 
 student_01 = {"student_id": 202101, "name": "Angelina", "Gender": "Female", "Age":18, "Scores": 120}
@@ -11,98 +9,127 @@ student_06 = {"student_id": 202106, "name": "Flora",    "Gender": "Female", "Age
 
 class_01 = {"student_01": student_01, "student_02": student_02, "student_03":student_03, "student_04": student_04, "student_05": student_05,
             "student_06": student_06}
-print(f"The whole class info: {class_01}")
+# 生成年纪的统计列表
+def get_age():
+    age = []
+    for std_info in class_01.values():
+        for key in std_info.keys():
+            if key == "Age":
+                age.append(std_info[key])
+    return age
 
+# 生成分数统计列表
+def get_scores():
+    scores = []
+    for std_info in class_01.values():
+        for key in std_info.keys():
+            if key == "Scores":
+                scores.append(std_info[key])
+    return scores
 
+# 生成男年纪统计列表：
+def get_male_stds_info():
+    male_age = []
+    for std_info in class_01.values():
+        if std_info['Gender'] == 'Male':
+            male_age.append(std_info["Age"])
+    return male_age
+
+# 生成男生分数统计列表：
+def get_male_scores():
+    male_scores = []
+    for std_info in class_01.values():
+        if std_info['Gender'] == 'Male':
+            male_scores.append(std_info["Scores"])
+    return male_scores
+
+# 生成女生年龄统计列表：
+def get_female_ages():
+    female_age = []
+    for std_info in class_01.values():
+        if std_info['Gender'] == 'Female':
+            female_age.append(std_info["Age"])
+    return female_age
+
+# 生成女生分数统计列表：
+def get_female_scores():
+    female_scores = []
+    for std_info in class_01.values():
+        if std_info['Gender'] == 'Female':
+            female_scores.append(std_info["Scores"])
+    return female_scores
 
 # 统计本班年纪最小/最大的年纪的学生信息：
-age = []
-for std_info in class_01.values():
-    for key in std_info.keys():
-        if key == "Age":
-            age.append(std_info[key])
-# print(age)
-# print(max(age))
-# print(min(age))
+def get_max_age_stds():
+    max_age_stds = {}
+    for std_number, std_details in class_01.items():
+        for key in std_details.keys():
+            if std_details[key] == max(get_age()):
+                max_age_stds[std_number] = std_details
+    print(f"The oldest student's info as below: {max_age_stds}")
 
-max_age_stds = {}
-min_age_stds = {}
-for std_number, std_details in class_01.items():
-    for key in std_details.keys():
-        if std_details[key] == max(age):
-            max_age_stds[std_number] = std_details
-        elif std_details[key] == min(age):
-            min_age_stds[std_number] = std_details
 
-print(f"The oldest student's info as below: {max_age_stds}")
-print(f"The youngest student's info as below: {min_age_stds}")
+def get_min_age_stds():
+    min_age_stds = {}
+    for std_number, std_details in class_01.items():
+        for key in std_details.keys():
+            if std_details[key] == min(get_age()):
+                min_age_stds[std_number] = std_details
+    print(f"The youngest student's info as below: {min_age_stds}")
 
 # 全班平均年龄
-class_age_avg = mean(age)
-print(f"The average age of the class is: {class_age_avg:.4}")
+def get_class_age_avg():
+    class_age_avg = sum(get_age()) / len(get_age())
+    print(f"The average age of the class is: {class_age_avg:.4}")
 
 # 统计分数最高/最低的学生信息：
-scores = []
-for std_info in class_01.values():
-    for key in std_info.keys():
-        if key == "Scores":
-            scores.append(std_info[key])
-# print(scores)
-# print(max(scores))
-# print(min(scores))
+def get_min_score_stds():
+    min_score_stds = {}
+    for std_number, std_details in class_01.items():
+        for key in std_details.keys():
+            if std_details[key] == min(get_scores()):
+                min_score_stds[std_number] = std_details
+    print(f"The info of student who got the lowest score as below: {min_score_stds}")
 
-max_score_stds = {}
-min_score_stds = {}
 
-for std_number, std_details in class_01.items():
-    for key in std_details.keys():
-        if std_details[key] == max(scores):
-            max_score_stds[std_number] = std_details
-        elif std_details[key] == min(scores):
-            min_score_stds[std_number] = std_details
-
-print(f"The info of student who got the highest score as below: {max_score_stds}")
-print(f"The info of student who got the lowest score as below: {min_score_stds}")
+def get_max_score_stds():
+    max_score_stds = {}
+    for std_number, std_details in class_01.items():
+        for key in std_details.keys():
+            if std_details[key] == max(get_scores()):
+                max_score_stds[std_number] = std_details
+    print(f"The info of student who got the highest score as below: {max_score_stds}")
 
 # 全班平均分
-class_scores_avg = mean(scores)
-print(f"The average score of the class: {class_scores_avg:.5}")
+def get_class_scores_avg():
+    class_scores_avg = sum(get_scores()) / len(get_scores())
+    print(f"The average score of the class: {class_scores_avg:.5}")
 
 # 男生平均年龄
-male_age = []
-for std_info in class_01.values():
-    if std_info['Gender'] == 'Male':
-        male_age.append(std_info["Age"])
-
-# print(male_age)
-# print(max(male_age))
-# print(min(male_age))
-print(f"The average age of the boys: {mean(male_age):.4}")
-
+def get_male_ages_avg():
+    print(f"The average age of the boys: {sum(get_male_stds_info())/len(get_male_stds_info()):.4}")
 
 # 男生平均分
-male_scores = []
-for std_info in class_01.values():
-    if std_info['Gender'] == 'Male':
-        male_scores.append(std_info["Scores"])
-# print(male_scores)
-print(f"The average scores of the boys: {mean(male_scores):.5}")
+def get_male_scores_avg():
+    print(f"The average scores of the boys: {sum(get_male_scores())/len(get_male_scores()):.5}")
 
 # 女生平均年龄
-female_age = []
-for std_info in class_01.values():
-    if std_info['Gender'] == 'Female':
-        female_age.append(std_info["Age"])
-
-# print(female_age)
-# print(max(female_age))
-# print(min(female_age))
-print(f"The average age of the girls: {mean(female_age):.4}")
+def get_female_ages_avg():
+    print(f"The average age of the girls: {sum(get_female_ages()) / len(get_female_ages()):.4}")
 
 # 女生平均分
-female_scores = []
-for std_info in class_01.values():
-    if std_info['Gender'] == 'Female':
-        female_scores.append(std_info["Scores"])
-# print(female_scores)
-print(f"The average score of the girls: {mean(female_scores):.5}")
+def get_female_scores_avg():
+    print(f"The average score of the girls: {sum(get_female_scores()) / len(get_female_scores()):.5}")
+
+
+if __name__ == "__main__":
+    get_min_age_stds()
+    get_max_age_stds()
+    get_class_age_avg()
+    get_min_score_stds()
+    get_max_score_stds()
+    get_class_scores_avg()
+    get_male_ages_avg()
+    get_male_scores_avg()
+    get_female_ages_avg()
+    get_female_scores_avg()
